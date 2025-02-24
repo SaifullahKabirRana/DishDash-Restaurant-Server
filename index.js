f97660002c7195b1990b134ad7b5efe6f9f0c77b
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-02-25 01:46:10 +0600

    implement cors credentials, origin

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
-
-app.use(cors());
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
