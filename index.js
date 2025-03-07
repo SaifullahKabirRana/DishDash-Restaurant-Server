a0542a19736a2bff37ba130e54aefb92855a1216
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-03-07 15:21:23 +0600

    update user role using patch method

--- index.js
+++ index.js
@@ -55,10 +55,23 @@ async function run() {
             res.send(result);
         })
 
+        // change user role
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updateDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
         // delete user data
         app.delete('/users/:id', async (req, res) => {
             const id = req.params.id;
-            const query  = {_id: new ObjectId(id)};
+            const query = { _id: new ObjectId(id) };
             const result = await userCollection.deleteOne(query);
             res.send(result);
         })
