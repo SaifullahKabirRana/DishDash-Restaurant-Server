5a2a09a52b1e16585c975d181f756db016414164
SaifullahKabirRana <saifullahkabirranaa@gmail.com>
2025-04-10 04:29:27 +0600

    added vercel json

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 
 // middleware
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://dish-dash-restaurant.vercel.app'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://dish-dash-restaurant.vercel.app', 'https://dishdash-restaurant.web.app'],
     credentials: true,
     optionSuccessStatus: 200,
 }
